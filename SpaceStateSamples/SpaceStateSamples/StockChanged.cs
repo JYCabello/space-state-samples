713795403a60ea79fcff8e758fe55763e68b5273
Yeray Cabello <j.cabello.ramos@gmail.com>
2023-03-29 14:33:59 +0200

    stock changed

---
+++ SpaceStateSamples/SpaceStateSamples/StockChanged.cs
@@ -0,0 +1,20 @@
+﻿namespace SpaceStateSamples;
+
+public record StockChanged(
+    Guid ProductID,
+    int Amount,
+    StockChangeReason Reason,
+    Guid? PurchaseID,
+    Guid? CustomerID,
+    Guid? ReportingUserID,
+    Guid? OrderID);
+
+public enum StockChangeReason
+{
+    Received,
+    Reserved,
+    ReserveExpired,
+    Shrinkage,
+    Ordered,
+    OrderCancelled
+}
